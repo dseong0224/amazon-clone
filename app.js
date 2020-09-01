d70ba6c567f0d0ae2044a81f533f31ab67d148bf
Daniel Seong <danielseong@daniels-mbp.lan>
2020-08-31 22:37:06 -0700

    successfully connected to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
-const app = express();
+// import mongoose
+const mongoose = require("mongoose");
+//lets us use .env file
 require("dotenv").config();
 
+//app
+const app = express();
+
+//connects to mongo db using mongoose and connection string specified in .env file
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+//routes
 app.get("/", (req, res) => {
-  res.send("hello from node live update");
+  res.send("hello from node with live update using nodemon");
 });
 
 const port = process.env.PORT || 8000;
