e86b22311a413dbb0a581b3b7f218a946e620453
Daniel Seong <danielseong@daniels-mbp.lan>
2020-09-07 01:35:50 -0700

    product delete product update category by id and single category category update delete and list all

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,23 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "Product not found",
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
@@ -14,9 +31,82 @@ exports.create = (req, res) => {
       });
     }
 
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "Missing one or more fields",
+      });
+    }
+
     let product = new Product(fields);
 
     if (files.photo) {
+      // console.log("FILES PHOTO: ", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1MB in size",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        console.log("Error creating product: ", err);
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      // deletedProduct,
+      message: "Product deleted successfully",
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (files.photo) {
+      // console.log("FILES PHOTO: ", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1MB in size",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create, categoryById, read, update, remove, list } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
+router.get("/categories", list);
 router.post("/category/create/:userId", requireSignin, isAdmin, isAuth, create);
+router.delete(
+  "/category/:categoryId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.put(
+  "/category/:categoryId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
 
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 
 module.exports = router;
