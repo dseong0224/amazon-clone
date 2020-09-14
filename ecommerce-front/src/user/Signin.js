b52317d5678a28a1139a5c408c3bfa927a84e664
Daniel Seong <danielseong@daniels-mbp.lan>
2020-09-14 14:21:02 -0700

    signup form handle change user signup user signup success and error refactored code

--- ecommerce-front/src/user/Signin.js
+++ ecommerce-front/src/user/Signin.js
@@ -1,10 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
+import { signin } from "../auth";
 
-const Signin = () => (
-  <Layout title="Signin Page" description="Signin to Node React E-commerce App">
-    ...
-  </Layout>
-);
+const Signin = () => {
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    redirectToReferrer: false,
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = userDetails;
+
+  const handleChange = (userDetail) => (event) => {
+    setUserDetails({
+      ...userDetails,
+      error: false, //resets error message at time of new input
+      [userDetail]: event.target.value,
+    });
+  };
+  // function handleChange (name){
+  //   function (event){
+  //     ...
+  //   }
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setUserDetails({ ...userDetails, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setUserDetails({ ...userDetails, error: data.error, loading: false });
+      } else {
+        setUserDetails({
+          ...userDetails,
+          redirectToReferrer: true,
+        });
+      }
+    });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-info col-md-12">
+        Sign In
+      </button>
+    </form>
+  );
+
+  const showErrorMessage = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className="alert alert-info">
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  return (
+    <Layout
+      title="Signup Page"
+      description="Signup with Node React E-commerce App"
+      className="container col-md-2 offset-md-5"
+    >
+      {showLoading()}
+      {showErrorMessage()}
+      {signUpForm()}
+      {redirectUser()}
+    </Layout>
+  );
+};
 
 export default Signin;
--- ecommerce-front/src/user/Signup.js
+++ ecommerce-front/src/user/Signup.js
@@ -1,14 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
-import API from "../config";
+import { signup } from "../auth";
 
-const Signup = () => (
-  <Layout
-    title="Signup Page"
-    description="Signup with Node React E-commerce App"
-  >
-    {API}
-  </Layout>
-);
+const Signup = () => {
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, success, error } = userDetails;
+
+  const handleChange = (userDetail) => (event) => {
+    setUserDetails({
+      ...userDetails,
+      error: false, //resets error message at time of new input
+      [userDetail]: event.target.value,
+    });
+  };
+  // function handleChange (name){
+  //   function (event){
+  //     ...
+  //   }
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setUserDetails({ ...userDetails, error: false });
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setUserDetails({ ...userDetails, error: data.error, success: false });
+      } else {
+        setUserDetails({
+          ...userDetails,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true,
+        });
+      }
+    });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-info col-md-12">
+        Sign Up
+      </button>
+    </form>
+  );
+
+  const showErrorMessage = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccessMessage = () => (
+    <div
+      className="alert alert-success"
+      style={{ display: success ? "" : "none" }}
+    >
+      New user account has been created. Click to{" "}
+      <Link to="/signin">
+        <strong>Sign In</strong>
+      </Link>
+    </div>
+  );
+
+  return (
+    <Layout
+      title="Signup Page"
+      description="Signup with Node React E-commerce App"
+      className="container col-md-2 offset-md-5"
+    >
+      {showSuccessMessage()}
+      {showErrorMessage()}
+      {signUpForm()}
+    </Layout>
+  );
+};
 
 export default Signup;
