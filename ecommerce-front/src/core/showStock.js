0795ccb13da2f762d4f37d7c10d9f8832b80458a
Daniel Seong <danielseong@daniels-mbp.lan>
2020-11-08 18:32:31 -0800

    fixed out of stock badge text

--- ecommerce-front/src/core/showStock.js
+++ ecommerce-front/src/core/showStock.js
@@ -4,8 +4,8 @@ const showStock = (quantity) => {
   return quantity > 0 ? (
     <span className="badge badge-light badge-pill text-success">In Stock.</span>
   ) : (
-    <span className="badge badge-secondary badge-pill text-danger">
-      Out of showStock.
+    <span className="badge badge-light badge-pill text-danger">
+      Out of Stock.
     </span>
   );
 };
